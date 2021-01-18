35b7ef5a1860b1f2e0deb79fed4b84c5d7cf7ffa
luoxin <1224084650@qq.com>
2021-01-18 12:54:19 +0800

    update api.

--- proposed/main_ratio.py
+++ proposed/main_ratio.py
@@ -13,9 +13,10 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if args.noise_scheme == None:
         print('None')
-        
+    print(args.noise_scheme)
+    print(args.outliers_scheme)
     parameters = {"ed": 8000, "R":6, "K":6, "mu":0, "sigma":0.1, 
-                "SNR":None, "noise_scheme": args.None, 'outliers_scheme': args.outliers_scheme}
+                "SNR":None, "noise_scheme": args.noise_scheme, 'outliers_scheme': args.outliers_scheme}
     start = time.time()
     eval_ratio('Abilene', parameters)
     end = time.time()
