309827f339bda27da3a2c33ec9804def7cd7ce6c
luoxin <1224084650@qq.com>
2021-01-18 14:05:46 +0800

    update api.

--- T-online/main_ratio.py
+++ T-online/main_ratio.py
@@ -14,9 +14,10 @@ if __name__ == '__main__':
     mu = 0
     sigma = 0.1
     SNR = None
+    init_scheme = 'svd'
     parameters = {"ed": 8000, "R":6, "mu":0, "sigma":0.1, "SNR": SNR, 
-                "noise_scheme": args.noise_scheme, 'outliers_scheme': args.outliers_scheme, 'init': 'svd'}
-    logging.info('ed: %d, R: %d, mu: %d, sigma: %f, noise_scheme: %s, outliers_scheme: %s, init: %s'%(ed, R, mu, sigma, args.noise_scheme, args.outliers_scheme))
+                "noise_scheme": args.noise_scheme, 'outliers_scheme': args.outliers_scheme, 'init': init_scheme}
+    logging.info('ed: %d, R: %d, mu: %d, sigma: %f, noise_scheme: %s, outliers_scheme: %s, init: %s'%(ed, R, mu, sigma, args.noise_scheme, args.outliers_scheme, init_scheme))
 
     start = time.time()
     eval_ratio('Abilene', parameters)
