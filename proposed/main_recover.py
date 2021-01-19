eac2c0e936121c096571d3cd19bcfdbdc197956c
luoxin <1224084650@qq.com>
2021-01-19 18:23:11 +0800

    update init R.

--- proposed/main_recover.py
+++ proposed/main_recover.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='T-online')
     parser.add_argument('--noise_scheme', type=str, default=None)
     parser.add_argument('--outliers_scheme', type=str, default=None)
-
+    parser.add_argument('--theta', type=float, default=0.1)
     args = parser.parse_args()
 
     ed = 8000
@@ -18,23 +18,22 @@ if __name__ == '__main__':
     K = 6
     mu = 0
     sigma = 0.1
-    theta = 0.1
     SNR = None
-    parameters = {"ed": ed, "R":R, "K":K, "mu":mu, "sigma":sigma, "theta": theta, "fraction": 0, 
+    parameters = {"ed": ed, "R":R, "K":K, "mu":mu, "sigma":sigma, "theta": args.theta, "fraction": 0, 
                 "SNR":SNR, "noise_scheme": args.noise_scheme, 'outliers_scheme': args.outliers_scheme}
     
-    logging.info('recover --- ed: %d, R: %d, K: %d, mu: %d, sigma: %f, theta: %f, noise_scheme: %s, outliers_scheme: %s'%(ed, R, K, mu, sigma, theta, args.noise_scheme, args.outliers_scheme))     
+    logging.info('recover --- ed: %d, R: %d, K: %d, mu: %d, sigma: %f, theta: %f, noise_scheme: %s, outliers_scheme: %s'%(ed, R, K, mu, sigma, args.theta, args.noise_scheme, args.outliers_scheme))     
     start = time.time()
     eval_missing_ratio('Abilene', parameters)
     end = time.time()
-    logging.info('Time consumption in Abilene on ratio: %f' %((end - start)/60))
+    logging.info('Time consumption in Abilene on missing_ratio: %f' %((end - start)/60))
 
     start = time.time()
     eval_missing_ratio('GEANT', parameters)
     end = time.time()
-    logging.info('Time consumption in GEANT on ratio: %f' %((end - start)/60))
+    logging.info('Time consumption in GEANT on missing_ratio: %f' %((end - start)/60))
 
     start = time.time()
     eval_missing_ratio('CERNET', parameters)
     end = time.time()
-    logging.info('Time consumption in CERNET on ratio: %f' %((end - start)/60))
+    logging.info('Time consumption in CERNET on missing_ratio: %f' %((end - start)/60))
