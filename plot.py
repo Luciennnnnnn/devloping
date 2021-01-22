005a1c4f777a8157e87c5eb0f59400e02b85afe3
luoxin <1224084650@qq.com>
2021-01-22 22:15:49 +0800

    update plot

--- plot.py
+++ plot.py
@@ -130,6 +130,60 @@ def plot_time(dataset_name, ed):
     plt.savefig('RSE.png')
     plt.clf()
 
+def plot_noise(dataset_name):
+    x = []
+    for i in range(10):
+        x.append(i / 10)
+    pre = os.path.join(os.path.join('results', dataset_name))
+
+    # with open(os.path.join(pre, os.path.join('proposed', 'noise_ER.json')), 'r') as FR:
+    #     y = json.loads(FR.read())
+
+    # with open(os.path.join(pre, os.path.join('proposed', 'noise_ER.json')), 'r') as FR:
+    #     y = json.loads(FR.read())
+
+    # with open(os.path.join(pre, os.path.join('proposed', 'noise_ER.json')), 'r') as FR:
+    #     y = json.loads(FR.read())
+
+    # with open(os.path.join(pre, os.path.join('proposed', 'noise_ER.json')), 'r') as FR:
+    #     y = json.loads(FR.read())
+
+    # with open(os.path.join(pre, os.path.join('proposed', 'noise_ER.json')), 'r') as FR:
+    #     y = json.loads(FR.read())
+    plt.figure(figsize=(11.5, 6.6))
+    # Gaussian anomaly enum anomaly ratio
+    # [1.218317, 1.231309, 1.201047, 1.219431, 1.131734, 1.171725]
+
+    # ER: Gaussian anomaly enum missing ratio from 0 ~ 0.9 Done 2021-01-21 22:51:40.log
+    y3 = [1.135438, 1.285860, 1.441335, 1.787816, 1.847997, 2.851324, 4.331003, 12.189432, 28.037928, 438.592552]
+
+    # ER: Gaussian noise enum missing ratio from 0 ~ 0.9 Done 2021-01-21 22:32:26.log
+    y2 = [1.174613, 1.301723, 1.533960, 1.977712, 2.038753, 3.141521, 4.522832, 9.745681, 28.832551, 338.805911]
+
+    # ER: Without noise and anomaly enum missing ratio from 0 ~ 0.9 Done 2021-01-21 22:30:36.log
+    y1 = [1.257989, 1.479484, 1.520001, 2.118648, 2.348242, 3.571217, 3.374463, 12.244402, 49.602915, 295.510673]
+    y4 = np.random.randn(10)
+    y5 = np.random.randn(10)
+    y6 = np.random.randn(10)
+
+    # ER: Gaussian noise and Structural anomaly enum missing ratio from 0 ~ 0.9 ?? 2021-01-21 22:50:44.log 好 ｜｜ 2021-01-22 09:42:32.log 坏
+    y7 = [1.187148, 1.381492, 1.499855, 2.084450, 1.653089, 2.005807, 3.901273, ]
+
+    plt.plot(range(len(x)), y1, color='#3d5d46', marker='o', linestyle='-', label='Without noise and anomaly')
+    plt.plot(range(len(x)), y2, color='#8dabb6', marker='s', linestyle='--', label='Gaussian noise')
+    plt.plot(range(len(x)), y3, color='#8dabb6', marker='s', linestyle='--', label='Random anomaly')
+    plt.plot(range(len(x)), y4, color='#8dabb6', marker='s', linestyle='--', label='Structural anomaly')
+    plt.plot(range(len(x)), y5, color='#ddccc5', marker='D', linestyle='-.', label='Gaussian noise and Random anomaly')
+    plt.plot(range(len(x)), y6, color='#ffa289', marker='h', linestyle=':', label='Gaussian noise and Exponential anomaly')
+    plt.plot(range(len(x)), y7, color='#ffa289', marker='h', linestyle=':', label='Gaussian noise and Structural anomaly')
+    plt.xticks(range(len(x)), x)
+    plt.legend()
+    plt.xlabel('Missing ratio', fontsize=12)
+    plt.ylabel('ER', fontsize=12)
+    plt.savefig('noise_ER.png', format='png')
+    plt.savefig('noise_ER.pdf', format='pdf')
+    plt.savefig('noise_ER.eps', format='eps')
+    plt.clf()
 
 def locations():
     x = range(ed)
@@ -151,5 +205,4 @@ def locations():
 
 
 if __name__ == '__main__':
-    print("changed")
-    locations()
+    plot_noise('Abilene')
